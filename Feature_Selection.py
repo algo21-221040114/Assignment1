77a29531a50487806f97acd6ed843aa50739a373
algo21-221040114 <221040114@link.cuhk.edu.cn>
2022-03-02 08:31:27 +0800

    edit_Feature_Selection

--- Feature_Selection.py
+++ Feature_Selection.py
@@ -1,10 +1,8 @@
 import pandas as pd
 import numpy as np
 
-para_year = ['2016', '2017', '2018', '2019', '2020', '2021']
 para_m = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
           40, 60, 80, 100, 120, 140, 160, 180, 200, 220, 240]
-
 
 # Feature selection for random forest
 
@@ -21,7 +19,7 @@ def intraday_ret(t):
 
 
 # Returns with respect to last closing price
-def ret_last_close(t):
+def close_ret(t):
     data_close = pd.read_csv('./Adj_close_price.csv', index_col='Date')
     data_cr_ret = pd.DataFrame(columns=data_close.columns, index=para_m)
     for i in range(data_cr_ret.shape[1]):
@@ -31,7 +29,7 @@ def ret_last_close(t):
 
 
 # Returns with respect to opening price:
-def ret_open(t):
+def open_ret(t):
     data_open = pd.read_csv('./Open_price.csv', index_col='Date')
     data_close = pd.read_csv('./Adj_close_price.csv', index_col='Date')
     data_or_ret = pd.DataFrame(columns=data_open.columns, index=para_m)
@@ -40,4 +38,18 @@ def ret_open(t):
             data_or_ret.iloc[j, i] = (data_open.iloc[t-1, i] / data_close.iloc[t-para_m[j]-1, i]) - 1
     return data_or_ret
 
+
 # Feature selection for LSTM
+def feature_lstm(t, stock_num):
+    f_lstm = []
+    for i in range(240):
+        # m = 1
+        f = [intraday_ret(t-i).iloc[0, stock_num], close_ret(t-i).iloc[0, stock_num], open_ret(t-i).iloc[0, stock_num]]
+        # Robust Scaler standardization
+        for j in range(len(f)):
+            f[j] = (f[j]-np.median(f))/(np.max(f)-np.min(f))
+        f_lstm.append(f)
+    return f_lstm
+
+
+
