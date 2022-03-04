b7af4518aa2e28d34a6958f46a061255b3b5a6ed
algo21-221040114 <221040114@link.cuhk.edu.cn>
2022-03-04 21:34:30 +0800

    edit_final

--- LSTM_Model.py
+++ LSTM_Model.py
@@ -1,34 +1,34 @@
 import pandas as pd
 import numpy as np
-import pickle
 from Feature_Selection import feature_lstm
 from keras.models import Sequential
-from keras.layers import Dense, Embedding
-# from keras.layers import CuDNNLSTM as LSTM
+from keras.layers import Dense
 from keras.layers import LSTM
 from keras.callbacks import EarlyStopping
+from Strategy_Performance import strategy_performance
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # only for Mac
 
 
+# Parameters
+train_min_t = 241  # the maximum lag is 240
+train_max_t = 754  # the first three years are for feature selection
+stock_num = 10
+
 # Training set, Target
-max_t = 242
 target = pd.read_csv('./Target.csv', index_col='Date')
 y = []
-for t in range(241, max_t):
+for t in range(train_min_t, train_max_t):
     target_series = target.iloc[t, :].values.T
     for i in range(len(target_series)):
         y.append([1-target_series[i], target_series[i]])
 y = np.array(y)
-print('over1')
 
 # Training set, Feature
-stock_num = 10
-x = np.zeros(((max_t-241)*stock_num, 240, 3))
-for t in range(241, max_t):
+x = np.zeros(((train_max_t-train_min_t)*stock_num, 240, 3))
+for t in range(train_min_t, train_max_t):
     for s in range(stock_num):
-        x[(t-241)*10+s] = np.array(feature_lstm(t, s))
-print('over2')
+        x[(t-train_min_t)*stock_num+s] = np.array(feature_lstm(t, s))
 
 # LSTM model
 model = Sequential()
@@ -37,27 +37,33 @@ model.add(Dense(2, activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='SGD', metrics=['accuracy'])
 callback = EarlyStopping(monitor='val_accuracy', patience=10, mode='max', restore_best_weights=True)
 model.fit(x, y, batch_size=32, validation_split=0.2, callbacks=[callback])
-print('over4')
 
 # Test set and Prediction
-t_test = 757
-x_test = np.zeros((stock_num, 240, 3))
-for s in range(stock_num):
-    x_test[s] = np.array(feature_lstm(t_test, s))
-prediction = model.predict(x_test)
-max_prob = prediction[0][1]
-min_prob = prediction[0][1]
-buy_order = 0
-sell_order = 0
-for i in range(1, len(prediction)):
-    prob = prediction[i][1]
-    if prob > max_prob:
-        max_prob = prob
-        buy_order = i
-    elif prob < min_prob:
-        min_prob = prob
-        sell_order = i
-print(buy_order, sell_order)
+test_max_t = 1006  # the forth year is for test
+buy_order_list = []
+sell_order_list = []
+for t in range(train_max_t, test_max_t):
+    x_test = np.zeros((stock_num, 240, 3))
+    for s in range(stock_num):
+        x_test[s] = np.array(feature_lstm(t, s))
+    prediction = model.predict(x_test)
+    max_prob = prediction[0][1]
+    min_prob = prediction[0][1]
+    buy_order = 0
+    sell_order = 0
+    for i in range(1, len(prediction)):
+        prob = prediction[i][1]
+        if prob > max_prob:
+            max_prob = prob
+            buy_order = i
+        elif prob < min_prob:
+            min_prob = prob
+            sell_order = i
+    buy_order_list.append(buy_order)
+    sell_order_list.append(sell_order)
+
+# Strategy Performance
+strategy_performance(buy_order_list, sell_order_list, train_max_t)
 
 
 
--- Random_Forest_Model.py
+++ Random_Forest_Model.py
@@ -2,6 +2,7 @@ from sklearn.ensemble import RandomForestClassifier
 from Feature_Selection import intraday_ret
 from Feature_Selection import close_ret
 from Feature_Selection import open_ret
+from Strategy_Performance import strategy_performance
 import pandas as pd
 
 # Parameters
@@ -9,17 +10,17 @@ train_min_t = 241  # the maximum lag is 240
 train_max_t = 754  # the first three years are for feature selection
 
 # Training set, Feature
-# for t in range(train_min_t, train_max_t):
-#     df1 = pd.DataFrame(intraday_ret(t).values.T)
-#     df2 = pd.DataFrame(close_ret(t).values.T)
-#     df3 = pd.DataFrame(open_ret(t).values.T)
-#     if t == train_min_t:
-#         x = pd.concat([df1, df2, df3], axis=1)
-#     else:
-#         df4 = pd.concat([df1, df2, df3], axis=1)
-#         x = x.append(df4)
-# # the feature data is big, reserve it to save time
-# x.to_csv('x.csv', sep=',', header=True, index=True)
+for t in range(train_min_t, train_max_t):
+    df1 = pd.DataFrame(intraday_ret(t).values.T)
+    df2 = pd.DataFrame(close_ret(t).values.T)
+    df3 = pd.DataFrame(open_ret(t).values.T)
+    if t == train_min_t:
+        x = pd.concat([df1, df2, df3], axis=1)
+    else:
+        df4 = pd.concat([df1, df2, df3], axis=1)
+        x = x.append(df4)
+# the feature data is big, reserve it to save time
+x.to_csv('x.csv', sep=',', header=True, index=True)
 x = pd.read_csv('./x.csv')
 x = x.iloc[:, 1:]
 
@@ -63,25 +64,8 @@ for t in range(754, test_max_t):
     sell_order_list.append(sell_order)
 
 # Strategy Performance
-data_open = pd.read_csv('./Open_price.csv', index_col='Date')
-data_close = pd.read_csv('./Adj_close_price.csv', index_col='Date')
-portfolio_ret = []
-cum_ret = 1
-daily_ret = 0
-avg_daily_ret = 0
-for i in range(len(buy_order_list)):
-    # if the stock number in buy_order, buy with open price and sell it with close price
-    up_ret = data_close.iloc[train_max_t+i, buy_order_list[i]]/data_open.iloc[train_max_t+i, buy_order_list[i]]-1
-    # if the stock number in sell_order, sell with open price and buy it with close price
-    down_ret = (data_open.iloc[train_max_t+i, sell_order_list[i]]/data_close.iloc[train_max_t+i, sell_order_list[i]])-1
-    daily_ret = (up_ret+down_ret)/2
-    avg_daily_ret += daily_ret
-    # cum_ret = cum_ret*(1+daily_ret)
-    portfolio_ret.append(daily_ret)
-avg_daily_ret = avg_daily_ret/len(buy_order_list)
-print(avg_daily_ret)
-# print(cum_ret)
-# print(len(portfolio_ret))
+strategy_performance(buy_order_list, sell_order_list, train_max_t)
+
 
 
 
