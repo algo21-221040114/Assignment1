4b03c7f470f49c0ebd0db5413ed35ee6399e105e
algo21-221040114 <221040114@link.cuhk.edu.cn>
2022-03-04 17:35:47 +0800

    edit_final

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Assignment1
-You can download the csv file through.
+
+This assignment refer to a paper, 'Forecasting directional movements of stock prices for intraday trading using LSTM and 
+random forests', written by Pushpendu Ghosh, Ariel Neufeld, Jajati Keshari Sahoo.
+You can access to the paper through https://paperswithcode.com/paper/forecasting-directional-movements-of-stock.
+
+The main proposal is to predict the up or down direction of stock prices, with Random Forest and LSTM.
+Data used in the assignment is obtained from the open database, Yahoo Finance.
+
+Due to equipment limit, this assignment is based on ten stocks from 2016.01.01 to 2019.12.31.
+If you have GPU or Cloud Sever, you can enlarge the stock basket, and extend time period to increase the training and test dataset. 
--- Random_Forest_Model.py
+++ Random_Forest_Model.py
@@ -4,39 +4,44 @@ from Feature_Selection import close_ret
 from Feature_Selection import open_ret
 import pandas as pd
 
-# Training set, Target
-target = pd.read_csv('./Target.csv', index_col='Date')
-for t in range(241, 754):
-    if t == 241:
-        y = pd.DataFrame(target.iloc[t, :].values.T)
-    else:
-        df1 = pd.DataFrame(target.iloc[t, :].values.T)
-        y = y.append(df1)
-print('over1')
+# Parameters
+train_min_t = 241  # the maximum lag is 240
+train_max_t = 754  # the first three years are for feature selection
 
 # Training set, Feature
-# for t in range(241, 754):
+# for t in range(train_min_t, train_max_t):
 #     df1 = pd.DataFrame(intraday_ret(t).values.T)
 #     df2 = pd.DataFrame(close_ret(t).values.T)
 #     df3 = pd.DataFrame(open_ret(t).values.T)
-#     if t == 241:
+#     if t == train_min_t:
 #         x = pd.concat([df1, df2, df3], axis=1)
 #     else:
 #         df4 = pd.concat([df1, df2, df3], axis=1)
 #         x = x.append(df4)
+# # the feature data is big, reserve it to save time
 # x.to_csv('x.csv', sep=',', header=True, index=True)
 x = pd.read_csv('./x.csv')
 x = x.iloc[:, 1:]
-print('over2')
+
+# Training set, Target
+target = pd.read_csv('./Target.csv', index_col='Date')
+for t in range(train_min_t, train_max_t):
+    if t == train_min_t:
+        y = pd.DataFrame(target.iloc[t, :].values.T)
+    else:
+        df1 = pd.DataFrame(target.iloc[t, :].values.T)
+        y = y.append(df1)
 
 # Random Forest Model
-clf = RandomForestClassifier(n_estimators=100, criterion='gini', max_depth=5, max_features=9,
+clf = RandomForestClassifier(n_estimators=100, criterion='gini', max_depth=4, max_features=9,
                              min_samples_split=2, random_state=0)
 clf.fit(x, y.values.ravel())
-# print(clf.feature_importances_)
 
 # Test set and Prediction
-for t in range(754, 755):
+test_max_t = 1006  # the forth year is for test
+buy_order_list = []
+sell_order_list = []
+for t in range(754, test_max_t):
     df1 = pd.DataFrame(intraday_ret(t).values.T)
     df2 = pd.DataFrame(close_ret(t).values.T)
     df3 = pd.DataFrame(open_ret(t).values.T)
@@ -54,7 +59,31 @@ for t in range(754, 755):
         elif prob < min_prob:
             min_prob = prob
             sell_order = j
-    print(buy_order, sell_order)
+    buy_order_list.append(buy_order)
+    sell_order_list.append(sell_order)
+
+# Strategy Performance
+data_open = pd.read_csv('./Open_price.csv', index_col='Date')
+data_close = pd.read_csv('./Adj_close_price.csv', index_col='Date')
+portfolio_ret = []
+cum_ret = 1
+daily_ret = 0
+avg_daily_ret = 0
+for i in range(len(buy_order_list)):
+    # if the stock number in buy_order, buy with open price and sell it with close price
+    up_ret = data_close.iloc[train_max_t+i, buy_order_list[i]]/data_open.iloc[train_max_t+i, buy_order_list[i]]-1
+    # if the stock number in sell_order, sell with open price and buy it with close price
+    down_ret = (data_open.iloc[train_max_t+i, sell_order_list[i]]/data_close.iloc[train_max_t+i, sell_order_list[i]])-1
+    daily_ret = (up_ret+down_ret)/2
+    avg_daily_ret += daily_ret
+    # cum_ret = cum_ret*(1+daily_ret)
+    portfolio_ret.append(daily_ret)
+avg_daily_ret = avg_daily_ret/len(buy_order_list)
+print(avg_daily_ret)
+# print(cum_ret)
+# print(len(portfolio_ret))
+
+
 
 
 
