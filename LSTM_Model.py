1f6063ac2f765c2172225eb043ff2c9364e6bf00
algo21-221040114 <221040114@link.cuhk.edu.cn>
2022-03-07 15:53:20 +0800

    edit_final

--- LSTM_Model.py
+++ LSTM_Model.py
@@ -10,60 +10,55 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # only for Mac
 
 
-# Parameters
-train_min_t = 241  # the maximum lag is 240
-train_max_t = 754  # the first three years are for feature selection
-stock_num = 10
+def lstm(train_min_t, train_max_t, test_max_t, stock_num, node1, node2):
+    # Training set, Target
+    target = pd.read_csv('./Target.csv', index_col='Date')
+    y = []
+    for t in range(train_min_t, train_max_t):
+        target_series = target.iloc[t, :].values.T
+        for i in range(len(target_series)):
+            y.append([1-target_series[i], target_series[i]])
+    y = np.array(y)
 
-# Training set, Target
-target = pd.read_csv('./Target.csv', index_col='Date')
-y = []
-for t in range(train_min_t, train_max_t):
-    target_series = target.iloc[t, :].values.T
-    for i in range(len(target_series)):
-        y.append([1-target_series[i], target_series[i]])
-y = np.array(y)
+    # Training set, Feature
+    x = np.zeros(((train_max_t-train_min_t)*stock_num, 240, 3))
+    for t in range(train_min_t, train_max_t):
+        for s in range(stock_num):
+            x[(t-train_min_t)*stock_num+s] = np.array(feature_lstm(t, s))
 
-# Training set, Feature
-x = np.zeros(((train_max_t-train_min_t)*stock_num, 240, 3))
-for t in range(train_min_t, train_max_t):
-    for s in range(stock_num):
-        x[(t-train_min_t)*stock_num+s] = np.array(feature_lstm(t, s))
+    # LSTM model
+    model = Sequential()
+    model.add(LSTM(node1, input_shape=(240, 3)))
+    model.add(Dense(node2, activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='SGD', metrics=['accuracy'])
+    callback = EarlyStopping(monitor='val_accuracy', patience=10, mode='max', restore_best_weights=True)
+    model.fit(x, y, batch_size=32, validation_split=0.2, callbacks=[callback])
 
-# LSTM model
-model = Sequential()
-model.add(LSTM(16, input_shape=(240, 3)))  # dropout=0.1, recurrent_dropout=0.1
-model.add(Dense(2, activation='softmax'))
-model.compile(loss='categorical_crossentropy', optimizer='SGD', metrics=['accuracy'])
-callback = EarlyStopping(monitor='val_accuracy', patience=10, mode='max', restore_best_weights=True)
-model.fit(x, y, batch_size=32, validation_split=0.2, callbacks=[callback])
+    # Test set and Prediction
+    buy_order_list = []
+    sell_order_list = []
+    for t in range(train_max_t, test_max_t):
+        x_test = np.zeros((stock_num, 240, 3))
+        for s in range(stock_num):
+            x_test[s] = np.array(feature_lstm(t, s))
+        prediction = model.predict(x_test)
+        max_prob = prediction[0][1]
+        min_prob = prediction[0][1]
+        buy_order = 0
+        sell_order = 0
+        for i in range(1, len(prediction)):
+            prob = prediction[i][1]
+            if prob > max_prob:
+                max_prob = prob
+                buy_order = i
+            elif prob < min_prob:
+                min_prob = prob
+                sell_order = i
+        buy_order_list.append(buy_order)
+        sell_order_list.append(sell_order)
 
-# Test set and Prediction
-test_max_t = 1006  # the forth year is for test
-buy_order_list = []
-sell_order_list = []
-for t in range(train_max_t, test_max_t):
-    x_test = np.zeros((stock_num, 240, 3))
-    for s in range(stock_num):
-        x_test[s] = np.array(feature_lstm(t, s))
-    prediction = model.predict(x_test)
-    max_prob = prediction[0][1]
-    min_prob = prediction[0][1]
-    buy_order = 0
-    sell_order = 0
-    for i in range(1, len(prediction)):
-        prob = prediction[i][1]
-        if prob > max_prob:
-            max_prob = prob
-            buy_order = i
-        elif prob < min_prob:
-            min_prob = prob
-            sell_order = i
-    buy_order_list.append(buy_order)
-    sell_order_list.append(sell_order)
-
-# Strategy Performance
-strategy_performance(buy_order_list, sell_order_list, train_max_t)
+    # Strategy Performance
+    strategy_performance(buy_order_list, sell_order_list, train_max_t)
 
 
 
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import datetime
 from Data_Preprocess import get_data
 from Target_Decision import target_decision
 from Random_Forest_Model import random_forest
-
+from LSTM_Model import lstm
 
 # Define the instruments to download.  randomly selected from S&P500
 # Define dataset time period
@@ -10,17 +10,24 @@ stock_basket = ['AAPL', 'FLS', 'GME', 'HRL', 'HSY', 'MSFT', 'SPG', 'SPY', 'TER',
 start_date = datetime.datetime(2016, 1, 4)
 end_date = datetime.datetime(2019, 12, 31)
 
-# Time Parameters
+# General Parameters
 train_min_t = 241  # the maximum lag is 240
 train_max_t = 754  # the first three years are for feature selection
 test_max_t = 1006  # the forth year is for test
+stock_num = 10  # total amount of stocks
 
 # Random Forest Model Parameters
 n_est = 100
 max_dep = 4
 max_fea = 9
 
+# LSTM Parameters
+node1 = 16  # LSTM parameter
+node2 = 2  # LSTM parameter
+
+
 if __name__ == "__main__":
     get_data(stock_basket, start_date, end_date)
     target_decision()
     random_forest(train_min_t, train_max_t, test_max_t, n_est, max_dep, max_fea)
+    lstm(train_min_t, train_max_t, test_max_t, stock_num, node1, node2)
